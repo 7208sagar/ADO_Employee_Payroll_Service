223a985299c707ca5f8a86ede6ccb359ba4078d8
sagar <7208sk@gmail.com>
2021-11-12 21:40:39 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADOEmployeePayroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Payroll Service Using ADO!");
+        }
+    }
+}
